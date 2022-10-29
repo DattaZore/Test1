e91dd5d83e1f218fa47bca37455e883442e4276a
Admin <Admin@DESKTOP-NGHQCF7>
2022-10-29 14:27:02 +0530

    First Commit1

---
+++ src/Basic/Class1.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Class1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Good Morning");
+     m1();
+     
+	}
+     public static void m1()
+     {
+    	 System.out.println("Hello");
+     }
+     
+     
+     
+	}
+
